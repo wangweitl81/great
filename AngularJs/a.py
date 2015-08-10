774a3e04edefa9cfadf246f261f19ad4a08e3175
unknown <wangweitl81@hotmail.com>
2015-08-10 11:50:01 +0800

    modified

--- AngularJs/a.py
+++ AngularJs/a.py
@@ -2,12 +2,12 @@ import urllib
 import urllib.request
 import json
 
-names = ['曹星','李宏','肖波','王洁琛','刘召清']
-ids = ['654226197703210014','362301196104210517','370321197609240017','362132198103150033','512224197211097726']
+names = ['刘树梅']
+ids = ['142324195809221020']
 
 url = 'http://esb.citicsinfo.com/open/accountx/customer/registerWithThreeElements.json'
 
-for x in range(0,5):
+for x in range(0,1):
 	values = dict(CUST_NM=names[x], DOC_TP_CODE='10100', DOC_NBR=ids[x])
 	data = urllib.parse.urlencode(values)
 	data = data.encode('utf-8') # data should be bytes
--- AngularJs/b.py
+++ AngularJs/b.py
@@ -1,4 +1,5 @@
 import json
-jsonstr = '{"RET_CODE":0,"RET_MESSAGE":"成功","RESULTSET":[{"SALE_AGNT_TP_CODE":"8888","ECIF_ID":"62000011797846","CUST_ST_CODE":"XA"}],"RECORD_NUM":1}'
+jsonstr = '{"RET_CODE":0,"RET_MESSAGE":"成功","RESULTSET":[{"SALE_AGNT_TP_CODE":\
+    "8888","ECIF_ID":"62000011797846","CUST_ST_CODE":"XA"}],"RECORD_NUM":1}'
 obj = json.loads(jsonstr)
 print(obj["RESULTSET"][0]['ECIF_ID'])
